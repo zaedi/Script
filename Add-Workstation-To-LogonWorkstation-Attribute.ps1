f717b73edf84208df89c5277db621722e243e49b
zaedi <zaedi.ahmed@gmail.com>
2023-04-03 15:39:54 +0600

    Add files via upload

---
+++ Add-Workstation-To-LogonWorkstation-Attribute.ps1
@@ -0,0 +1,13 @@
+﻿$computers = Get-ADComputer -Filter *
+
+foreach ($computer in $computers) {
+    $employeeId = $computer.Name.Split("-")[-2]
+    $user = Get-ADUser -Filter "EmployeeID -eq '$employeeId'"
+    if ($user) {
+        $logonWorkstations = $user.LogonWorkstations
+        if ($logonWorkstations -notcontains $computer.Name) {
+            $logonWorkstations += $computer.Name
+            Set-ADUser -Identity $user -LogonWorkstations $logonWorkstations
+        }
+    }
+}
